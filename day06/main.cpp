d2ca06d5d94cad80341b151fb4c87d9c4a1f2e2e
Adrian WEAVER <aweaver@student.42.fr>
2022-12-06 16:25:21 +0100

    ADD DAY06 STAR2:
    Adding working solution for day06 second star.

--- day06/main.cpp
+++ day06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 07:56:02 by aweaver           #+#    #+#             */
-/*   Updated: 2022/12/06 12:13:43 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/12/06 16:23:54 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,20 @@ int main(void)
 		for (unsigned int i = 0; i < buffer.size() && i < buffer.size() - 4; i++)
 		{
 			int duplicate = 0;
-			for (unsigned int j = 0; j < 3; j++)
+			for (unsigned int j = 0; j < 13; j++)
 			{
 				if (duplicate == 1)
 					break;
-				for (unsigned int k = j + 1; k < 4; k++)
+				for (unsigned int k = j + 1; k < 14; k++)
 				{
 					if (buffer[i + k] == buffer[i + j])
 					{
 						duplicate = 1;
 						break;
 					}
-					else if (k == 3 && j == 2)
+					else if (k == 13 && j == 12)
 					{
-						std::cout << "message at index: " << i << std::endl;
+						std::cout << "message at index: " << i + 14 << std::endl;
 						return (0);
 					}
 				}
